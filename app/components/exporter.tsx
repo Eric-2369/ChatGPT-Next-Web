dacbae7f6eb41c9578da5a44aa4f573c5ddb90b0
Eric-2369 <admin@eri.cx>
2023-12-26 15:15:20 +0800

    refactor: rename project from NextChat back to ChatGPT Next Web

--- app/components/exporter.tsx
+++ app/components/exporter.tsx
@@ -524,7 +524,7 @@ export function ImagePreviewer(props: {
           </div>
 
           <div>
-            <div className={styles["main-title"]}>NextChat</div>
+            <div className={styles["main-title"]}>ChatGPT Next Web</div>
             <div className={styles["sub-title"]}>
               github.com/ChatGPTNextWeb/ChatGPT-Next-Web
             </div>
